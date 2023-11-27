85480b7b06e71a3d4f9334895b05ae0bb124d7c0
Florian Kraft <schnuffifk@gmail.com>
2023-11-27 20:39:41 +0100

    moves to new styling and fixes deprecation

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,26 +1,33 @@
 local lsp = require('lsp-zero')
-
-lsp.preset('recommended')
--- lsp.ensure_installed({
--- 	'tsserver',
--- 	'rust_analyzer',
--- })
-
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
+
+cmp.setup({
+    window = {
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+
+        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior =cmp.SelectBehavior.Select}),
+        ['<C-n>'] = cmp.mapping.select_next_item({ behavior =cmp.SelectBehavior.Select}),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        -- ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        -- ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+        -- ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        -- ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    })
 })
+lsp.preset('recommended')
 
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
--- lsp.setup_nvim_cmp({
--- 	mapping = cmp_mappings
--- })
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    -- Replace the language servers listed here 
+    -- with the ones you want to install
+    ensure_installed = {'tsserver', 'rust_analyzer'},
+    handlers = {
+        lsp.default_setup,
+    },
+})
 
 lsp.set_preferences({
 	suggest_lsp_servers = false,
