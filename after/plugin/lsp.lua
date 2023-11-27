ab2c3c93fe2fec60bed498a4c60638e10e9291ca
Florian Kraft <schnuffifk@gmail.com>
2023-11-27 20:21:43 +0100

    removes deprecation warnings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,10 +1,10 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
-lsp.ensure_installed({
-	'tsserver',
-	'rust_analyzer',
-})
+-- lsp.ensure_installed({
+-- 	'tsserver',
+-- 	'rust_analyzer',
+-- })
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
@@ -18,9 +18,9 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
 
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
-})
+-- lsp.setup_nvim_cmp({
+-- 	mapping = cmp_mappings
+-- })
 
 lsp.set_preferences({
 	suggest_lsp_servers = false,
