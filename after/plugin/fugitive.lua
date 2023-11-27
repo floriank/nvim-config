3c336e251098dd43c652d6ea62b46f16916de9cb
Florian Kraft <schnuffifk@gmail.com>
2023-11-27 21:10:54 +0100

    corrects mapping

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,2 +1,2 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
-vim.keymap.set('n', '<leader>gP', vim.cmd.GitPush)
+vim.keymap.set('n', '<leader>gP', ":Git push")
---
+++ after/plugin/vim-github-link.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("x","<leader>gl", ":GetCommitLink<CR>")
+
